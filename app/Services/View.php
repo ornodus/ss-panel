af8a689cd4eeee80643fba84b6e2d294845c079a
ornodus <ornode@post.usts.edu.cn>
2016-02-27 01:25:47 +0800

    update for gae

--- app/Services/View.php
+++ app/Services/View.php
@@ -9,8 +9,8 @@ class View
     public static function getSmarty(){
         $smarty=new smarty(); //实例化smarty
         $smarty->settemplatedir(BASE_PATH.'/resources/views/'.Config::get('theme').'/'); //设置模板文件存放目录
-        $smarty->setcompiledir(BASE_PATH.'/storage/framework/smarty/compile/'); //设置生成文件存放目录
-        $smarty->setcachedir(BASE_PATH.'/storage/framework/smarty/cache/'); //设置缓存文件存放目录
+        $smarty->setcompiledir("gs://sound-url.appspot.com/achievotmp/compiled/"); //设置生成文件存放目录
+        $smarty->setcachedir("gs://sound-url.appspot.com/achievotmp/cache/"); //设置缓存文件存放目录
         // add config
         $smarty->assign('config',Config::getPublicConfig());
         $smarty->assign('user',Auth::getUser());
